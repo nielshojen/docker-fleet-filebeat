fa8d03e7bf7ab40d54a42b63134ce3ada69bb2db
Niels Højen <niels@hojen.net>
2019-03-12 22:15:21 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-/usr/bin/fleet serve
-
 if [ -f /etc/filebeat/filebeat.yml ]; then
   echo "Found config file at /etc/filebeat/filebeat.yml"
-  /usr/bin/filebeat -e -c /etc/filebeat/filebeat.yml
+  /usr/bin/filebeat -e -c /etc/filebeat/filebeat.yml &
 else
   echo "No config file found at /etc/filebeat/filebeat.yml"
 fi
+
+/usr/bin/fleet serve
