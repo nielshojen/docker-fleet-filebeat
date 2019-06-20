6e48dd3907c1c38c7964f7ed5e579b4c210884f3
Niels Højen <niels.hojen@bestseller.com>
2019-06-20 09:58:56 +0200

    Added the stout logging again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ -f /etc/filebeat/filebeat.yml ]; then
   echo "Found config file at /etc/filebeat/filebeat.yml"
-  /usr/bin/filebeat -c /etc/filebeat/filebeat.yml &
+  /usr/bin/filebeat -e -c /etc/filebeat/filebeat.yml &
 else
   echo "No config file found at /etc/filebeat/filebeat.yml"
 fi
